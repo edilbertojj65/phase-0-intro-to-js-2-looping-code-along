863f8313efd2d0a747dd2be2e388c45d7e700341
edilbertojj65 <edilbertojj65@gmail.com>
2021-03-20 20:01:32 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solutions in this file
+
+
+
+function writeCards(names, event){
+
+     const messages = []
+
+    for ( let i = 0; i < names.length; i++){
+   
+         // console.log(`Thank you ${array[i]} for the //wonderful surprise gift!`);
+   
+          const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+          messages.push(message);
+     }
+      return messages
+   }
+ 
+   function countDown(number){
+        for (let i = number ; i > 0; i--){
+
+          console.log(i);
+        }
+        console.log(0);   
+     
+     }
+
+   
